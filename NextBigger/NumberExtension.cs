11fcff1fc9d92d78c1d21a0a41a175454637810d
Glouzzzdus <91818237+Glouzzzdus@users.noreply.github.com>
2022-12-15 15:50:15 +0200

    Add files via upload
    Ok

---
+++ NextBigger/NumberExtension.cs
@@ -0,0 +1,56 @@
+﻿namespace NextBiggerTask
+{
+    public static class NumberExtension
+    {
+        /// <summary>
+        /// Finds the nearest largest integer consisting of the digits of the given positive integer number; return -1 if no such number exists.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>
+        /// The nearest largest integer consisting of the digits  of the given positive integer; return -1 if no such number exists.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown when source number is less than 0.</exception>
+        public static int NextBiggerThan(int number)
+        {
+            if (number == int.MaxValue)
+            {
+                return -1;
+            }
+            else if (number < 0)
+            {
+                throw new ArgumentException($"Value of {nameof(number)} cannot be less zero.", nameof(number));
+            }
+
+            int a = number % 10;
+            int rests = 0;
+            for (double i = 0; number > 0; i++)
+            {
+                int b = number % 10;
+                if (b < a)
+                {
+                    break;
+                }
+                else
+                {
+                    rests += b * (int)Math.Pow(10, i);
+                    a = b;
+                    number /= 10;
+                }
+            }
+
+            if (number == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                var itemToChange = number % 10;
+                int itemForChange = NumberExtensionHelpers.GetBiggerThan(itemToChange, rests);
+                number = ((number / 10) * 10) + itemForChange;
+                rests = NumberExtensionHelpers.SortNumbers(NumberExtensionHelpers.SwapDigitToLess(itemToChange, rests), -1);
+
+                return NumberExtensionHelpers.AppendNumbers(number, rests);
+            }
+        }
+    }
+}
---
+++ NextBigger/NumberExtensionHelpers.cs
@@ -0,0 +1,99 @@
+﻿namespace NextBiggerTask
+{
+    internal static class NumberExtensionHelpers
+    {
+        public static int AppendNumbers(int number1, int number2)
+        {
+            while (number2 > 0)
+            {
+                number1 = (number1 * 10) + (number2 % 10);
+                number2 /= 10;
+            }
+
+            return number1;
+        }
+
+        public static int SwapDigitToLess(int itemToChange, int number)
+        {
+            int rest = 0;
+            while (number > 0)
+            {
+                int c = number % 10;
+                if (c > itemToChange)
+                {
+                    number = ((number / 10) * 10) + itemToChange;
+                    break;
+                }
+
+                rest = (rest * 10) + c;
+                number /= 10;
+            }
+
+            while (rest > 0)
+            {
+                number = (number * 10) + (rest % 10);
+                rest /= 10;
+            }
+
+            return number;
+        }
+
+        public static int GetBiggerThan(int itemToChange, int number)
+        {
+            int itemForChange = 0;
+            var sortedRests = SortNumbers(number, -1);
+            while (sortedRests > 0)
+            {
+                int c = sortedRests % 10;
+                if (c > itemToChange)
+                {
+                    itemForChange = c;
+                    break;
+                }
+
+                sortedRests /= 10;
+            }
+
+            return itemForChange;
+        }
+
+        public static int SortNumbers(int x, int v)
+        {
+            var input = x;
+            int output = 0;
+            bool sorted = true;
+            if (x < 10)
+            {
+                return x;
+            }
+
+            while (sorted)
+            {
+                int mod = 1;
+                sorted = false;
+
+                while (input > 10)
+                {
+                    var digit1 = input % 10;
+                    var digit2 = (input / 10) % 10;
+
+                    if (digit1 != digit2 && Math.Sign(digit1 - digit2) != Math.Sign(v))
+                    {
+                        sorted = true;
+                        input = ((input / 100) * mod * 100) + (output % mod) + (digit1 * mod * 10) + (digit2 * mod);
+                        break;
+                    }
+                    else
+                    {
+                        output = (output % mod) + (digit2 * mod * 10) + (digit1 * mod);
+                    }
+
+                    input = input / 10;
+                    mod *= 10;
+                }
+            }
+
+            return output;
+        }
+    }
+}
